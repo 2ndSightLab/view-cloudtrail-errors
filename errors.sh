b96d85f14182fc70298ad983108aa382f8c68ee2
Teri Radichel <github@pwn.fyi>
2025-09-07 19:21:21 -0400

    mac fix
    mac fix

--- errors.sh
+++ errors.sh
@@ -6,7 +6,10 @@ read -p "enter profile: " profile
 read -p "enter region: " region
 read -p "enter how many minutes you want to retrieve (e.g. 5 or 10): " minutes
 
+# On Linux
 START_TIME=$(date -u --date "$minutes minutes ago" '+%Y-%m-%dT%H:%M:%SZ')
+# On a Mac, per Michael Speer, Xpertloop (I haven't tested this as I do not install the AWS CLI locally)
+#START_TIME=$(date -u -v-${minutes}M'+%Y%m-$dT%H:%M:%SZ')
 
 echo ""
 echo "CLOUDTRAIL ERRORS ONLY"
